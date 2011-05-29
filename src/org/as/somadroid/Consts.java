95ff6844a9982ae8ecc06e6f35595acd2dde1182
Andrea Stagi <stagi.andrea@gmail.com>
2011-05-29 19:16:05 +0200

    Right indentation in Consts

--- src/org/as/somadroid/Consts.java
+++ src/org/as/somadroid/Consts.java
@@ -30,9 +30,9 @@ package org.as.somadroid;
 
 public class Consts {
 	
-	public static final String CHANNELS_URL = "http://somafm.com/channels.xml";
-	public static final int SONG_LIST_MAXSIZE = 10;
-	public static final int REFRESH_DELAY = 5000;
+    public static final String CHANNELS_URL = "http://somafm.com/channels.xml";
+    public static final int SONG_LIST_MAXSIZE = 10;
+    public static final int REFRESH_DELAY = 5000;
     public static final int CLOSE_REQUEST = 10;
 	
 }
