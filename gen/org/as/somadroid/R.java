e47fd21595668212361ac988016fb80903678ff2
Andrea Stagi <stagi.andrea@gmail.com>
2011-06-08 02:18:00 +0200

    Added resources

--- gen/org/as/somadroid/R.java
+++ gen/org/as/somadroid/R.java
@@ -12,10 +12,11 @@ public final class R {
     }
     public static final class drawable {
         public static final int background=0x7f020000;
-        public static final int icon=0x7f020001;
-        public static final int notification_icon=0x7f020002;
-        public static final int play=0x7f020003;
-        public static final int stop=0x7f020004;
+        public static final int backgroundl=0x7f020001;
+        public static final int icon=0x7f020002;
+        public static final int notification_icon=0x7f020003;
+        public static final int play=0x7f020004;
+        public static final int stop=0x7f020005;
     }
     public static final class id {
         public static final int about=0x7f050011;
